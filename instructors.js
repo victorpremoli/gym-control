d242132a83e21867ce3d5aca8d148015948cc449
Victor Premoli <victor.premoli57@gmail.com>
2020-11-26 19:05:18 -0300

    first commit

---
+++ instructors.js
@@ -0,0 +1,78 @@
+const fs = require('fs')
+const data = require('./data.json')
+const { age } = require('./utils')
+
+//show
+exports.show = function(req,res){
+    const { id } = req.params
+
+    const foundInstructor = data.instructors.find(function(instructor) {
+        return instructor.id == id
+    })
+
+    if (!foundInstructor) return res.send("Instructor not found")
+
+    
+    const instructor = {
+        ...foundInstructor,
+        age:age(foundInstructor.birth),
+        services: foundInstructor.services.split(','),
+        created_at: Intl.DateTimeFormat("pt-BR").format(foundInstructor.created_at),
+    }
+
+    return res.render('instructors/show', {instructor})
+}
+// create 
+exports.post = function(req, res) {
+
+    const keys = Object.keys(req.body)
+
+    for (key of keys) {
+        if(req.body[key] == "") {
+            return res.send('please fill all fields!!') 
+        }
+    }
+
+    let {avatar_url, birth, gender, services, name} = req.body
+
+    birth = Date.parse(req.body.birth)
+    const created_at = Date.now()
+    const id = Number(data.instructors.length + 1)
+
+
+    data.instructors.push({
+        id, 
+        avatar_url,
+        name,
+        birth, 
+        gender, 
+        services,
+        created_at 
+    })
+
+    
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
+        if (err) {
+            return res.send ('write file error')
+        }
+        return res.redirect('/instructors')
+
+    })
+    
+    //return res.send(req.body)
+}
+// update
+exports.edit = function(req, res) {
+
+    const { id } = req.params
+
+    const foundInstructor = data.instructors.find(function(instructor) {
+        return instructor.id == id
+    })
+
+    if (!foundInstructor) return res.send("Instructor not found")
+
+    return res.render('instructors/edit', {instructor : foundInstructor})
+}
+
+//delete
